0c80b3c2e54b535cd3d51c62fc0f63f6ce5aa357
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-11 20:49:45 +0530

    modules: add maintenance module

---
+++ src/modules/maintenance.ts
@@ -0,0 +1,20 @@
+import { Message, Snowflake } from "discord.js";
+
+let authorizedChannels: Snowflake[] = [
+    "267022940967665664"
+];
+
+export default async (message: Message) => {
+    if (message.author.bot) return;
+
+    if (message.member && message.member.hasPermission("MANAGE_MESSAGES")) return;
+
+    if (authorizedChannels.includes(message.channel.id)) {
+        if (/^[#!~%]+[a-z0-9]+/.test(message.content)) {
+            if (message.deletable) await message.delete().catch(() => {});
+
+            let cleanupMessage = await message.channel.send(`Hey **${message.author.tag}**! Commands are disabled in this channel. Please go to <#268826070378217473> and use the \`#!\` prefix to use the commands.`).catch(() => {}) as Message;
+            if (cleanupMessage) await cleanupMessage.delete(10000).catch(() => {});
+        }
+    }
+};
